20a377674e5990bec4e4c698cdf94772cb7bfed9
Ilya Titov <titov.iiu@phystech.edu>
2020-10-05 15:17:22 +0300

    Рефакторинг

--- lab4/3_3.py
+++ lab4/3_3.py
@@ -1,46 +1,56 @@
 import pygame
 from pygame.draw import *
+ 
 
-def guy(people , x):
+def guy(people , x, y):
     '''
         The function draws people 
         
         people - type of people
         x - coordinate of people
+        y - coordinate of head( for female and male is head_y, for child is head_y_child)
     '''
     k = 0
+    feet(people, x, y,k )
+    
+
     if people == 'male':
-        ellipse(screen, pygame.Color(167, 147, 172), ((x - 3 * h // 22, head_y), (3 * h // 11, 6 * h // 11))) # draw body
-        arms(people, x)
+        ellipse(screen, pygame.Color(167, 147, 172), ((x - 3 * h // 22, y), (3 * h // 11, 6 * h // 11))) # draw body
         k = h // 22
     if people == 'female':
         polygon(screen, pygame.Color(255, 85, 221), ((x, head_y),
         (x - 3 * h // 22, head_y + 6 * h // 11 - h // 55), (x + 3 * h // 22, head_y + 6 * h // 11 - h // 55)))    #bodies
-        arms(people, x)
+        
     if people == 'child_female':
         polygon(screen, pygame.Color(255, 85, 221), ((x, head_y_child),
-            (x - 3 * h // 44, head_y_child + 6 * h // 22 - h // 55), (x + 3 * h // 44, head_y_child + 6 * h // 22 - h // 55)))
-        arms(people, x)
+            (x - 3 * h // 44, head_y_child + 6 * h // 22 - h // 55), (x + 3 * h // 44, head_y_child + 6 * h // 22 - h // 55))) 
     if people == 'child_male':
-        ellipse(screen, pygame.Color(167, 147, 172), ((x - 3 * h // 44, head_y_child), (3 * h // 22, 6 * h // 22))) # draw body
-        arms(people, x)
+        ellipse(screen, pygame.Color(167, 147, 172), ((x - 3 * h // 44, head_y_child), (3 * h // 22, 6 * h // 22))) # draw body  
         k = h // 44
-
+        
+    head(people, x, y)
+    arms(people, x)
 
    
+def feet(people, x, y,k):
     if (people == 'female') or (people == 'male'):
-        circle(screen, pygame.Color(244, 227, 215), (x, head_y - 4 * h // 55), h // 10)   #making heads
-        line(screen, pygame.Color("BLACK"), (x + h // 22, head_y + 6 * h // 11 - h // 55), (x + h // 22 + k // 3, head_y + 9 * h // 11))              
-        line(screen, pygame.Color("BLACK"), (x - h // 22, head_y + 6 * h // 11 - h // 55), (x - h // 22 - k, head_y + 9 * h // 11))
-        line(screen, pygame.Color("BLACK"), (x + h // 22 + k // 3, head_y + 9 * h // 11), (x + h // 22 + 4 * h // 55 + k // 3, head_y + 9 * h // 11))
-        line(screen, pygame.Color("BLACK"), (x - h // 22 - k, head_y + 9 * h // 11), (x - h // 22 - 4 * h // 55 - k, head_y + 9 * h // 11)) 
+        line(screen, pygame.Color("BLACK"), (x + h // 22, y + 6 * h // 11 - h // 55), (x + h // 22 + k // 3, y + 9 * h // 11))              
+        line(screen, pygame.Color("BLACK"), (x - h // 22, y + 6 * h // 11 - h // 55), (x - h // 22 - k, y + 9 * h // 11))
+        line(screen, pygame.Color("BLACK"), (x + h // 22 + k // 3, y + 9 * h // 11), (x + h // 22 + 4 * h // 55 + k // 3, y + 9 * h // 11))
+        line(screen, pygame.Color("BLACK"), (x - h // 22 - k, y + 9 * h // 11), (x - h // 22 - 4 * h // 55 - k, y + 9 * h // 11)) 
 
     if people == 'child_male' or people == 'child_female':
-        circle(screen, pygame.Color(244, 227, 215), (x, head_y_child - 2 * h // 55), h // 20)   #making child heads
-        line(screen, pygame.Color("BLACK"), (x + h // 22, head_y_child +  2.5* h // 11 ), (x + h // 22 + k // 3, head_y_child + 4 * h // 11))              
-        line(screen, pygame.Color("BLACK"), (x - h // 22, head_y_child + 2.5* h // 11 - h // 55), (x - h // 22 - k, head_y_child + 4 * h // 11))
-        line(screen, pygame.Color("BLACK"), (x + h // 22 + k // 3, head_y_child + 4 * h // 11), (x + h // 22 + 4 * h // 55 + k // 3, head_y_child + 4 * h // 11))
-        line(screen, pygame.Color("BLACK"), (x - h // 22 - k, head_y_child + 4 * h // 11), (x - h // 22 - 4 * h // 55 - k, head_y_child + 4 * h // 11)) 
+        line(screen, pygame.Color("BLACK"), (x + h // 22, y +  2.5* h // 11 ), (x + h // 22 + k // 3, y + 4 * h // 11))              
+        line(screen, pygame.Color("BLACK"), (x - h // 22, y + 2.5* h // 11 - h // 55), (x - h // 22 - k, y + 4 * h // 11))
+        line(screen, pygame.Color("BLACK"), (x + h // 22 + k // 3, y + 4 * h // 11), (x + h // 11 + k // 3, y + 4 * h // 11))
+        line(screen, pygame.Color("BLACK"), (x - h // 22 - k, y + 4 * h // 11), (x - h // 11 - k, y+ 4 * h // 11)) 
+
+def head(people,x,y ):
+    if (people == "child_male") or (people == "child_female"):
+        circle(screen, pygame.Color(244, 227, 215), (x, y - 2 * h // 55), h // 20)
+    else:
+         circle(screen, pygame.Color(244, 227, 215), (x, y - 4 * h // 55), h // 10)
+        
 
 def arms(people, x):
     '''
@@ -55,7 +65,18 @@ def arms(people, x):
     elif people == "male":
         line(screen, pygame.Color("BLACK"), (x - 4 * h // 55, head_y + 4 * h // 55), (x - 16 * h // 55, head_y + 19 * h // 55))              
         line(screen, pygame.Color("BLACK"), (x + 4 * h // 55, head_y + 4 * h // 55), (x + 16 * h // 55 , head_y + 19 * h // 55))    #making arms
-   
+        if x == p[0]: #draw arm of fist male
+            line(screen, pygame.Color("BLACK"), (p[0] - 16 * h // 55, head_y + 19 * h // 55), (p[0] - 16 * h // 55 - 10, head_y + 19 * h // 55 - 80))
+
+    elif people == "child_female":
+        line(screen, pygame.Color("BLACK"), (x - h // 55, head_y_child + 4 * h // 55), (x - 5*h // 55, head_y_child + 12 * h // 55))              
+        line(screen, pygame.Color("BLACK"), (x +  h // 55, head_y_child + 4 * h // 55), (x + 5 * h // 55, head_y_child + 12 * h // 55))  
+
+    elif people == "child_male":
+        line(screen, pygame.Color("BLACK"), (x - 3*  h // 55, head_y_child + 4 * h // 55), (x - 5* h // 55, head_y_child + 12* h // 55))              
+        line(screen, pygame.Color("BLACK"), (x +  3*h // 55, head_y_child + 4 * h // 55), (x + 5* h // 55, head_y_child + 12 * h // 55))  
+
+    
 
 def ice_cream(x, y, side, k):
  
@@ -79,31 +100,31 @@ FPS = 30
 HIGHT = 768
 WIDTH = 1032
 screen = pygame.display.set_mode((WIDTH, HIGHT))
-p = [180, 250, 300, 395, 635, 720, 790, 860]    # list of  people's x coordinate 
+p = [180, 250, 330, 395, 635, 720, 790, 860]    # list of  people's x coordinate 
 
 h = 350    # height of people
 head_y = 240    #y coordinate  of heads
-head_y_child = 360 
+head_y_child = 400 
 f = 1    #flag for arms of the 2nd woman
 
 screen.fill(pygame.Color(55, 200, 113))
 rect(screen, pygame.Color(170, 238, 255), (0, 0, WIDTH, HIGHT // 2))    #making sky and grass
 
-guy('male', p[0]) 
-guy('female', p[3])
-guy('child_male', p[1])
-guy('child_male', p[2])
+guy('male', p[0],head_y) 
+guy('female', p[3],head_y)
+guy('child_male', p[1], head_y_child)
+guy('child_male', p[2], head_y_child)
 
 
 f = -1
-guy('female', p[4])
-guy('child_female', p[5])
-guy('child_female', p[6])
-guy('male', p[7])    #making men and women
+guy('female', p[4],head_y)
+guy('child_female', p[5],head_y_child)
+guy('child_female', p[6],head_y_child)
+guy('male', p[7],head_y)    #making men and women
 
 ice_cream(p[7] + 16 * h // 55, head_y + 19 * h // 55, 1, 1)
-line(screen, pygame.Color("BLACK"), (p[0] - 16 * h // 55, head_y + 19 * h // 55), (p[0] - 16 * h // 55 - 10, head_y + 19 * h // 55 - 80))
 heart(p[0] - 16 * h // 55 - 10, head_y + 19 * h // 55 - 80, -1, 1)
+# делаем соединение мороженного и рук
 line(screen, pygame.Color("BLACK"), (p[3] + 4 * h // 11 - 9, head_y + 4 * h // 55), (p[3] + 4 * h // 11 - 9 - 10, head_y + 4 * h // 55 - 110))
 ice_cream(p[3] + 4 * h // 11 - 9 - 10, head_y + 4 * h // 55 - 110, -1, 2)
 
